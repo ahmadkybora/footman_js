08960abb1de51264cfc26e9a7cdc078f32df90f5
ahmad montazeri <amontazeri53@yahoo.com>
2021-01-29 21:25:48 +0330

    new

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,14 +7,48 @@ function getUserNumberInput()
      return parseInt(userInput.value)
 }
 
-function add()
+function crateAndWriteOutput(operator, resultBeforeCalc, calcNumber)
 {
-    const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
-    result = currentResult + parseInt(userInput.value());
+    const calcDescription = `${resultBeforeCalc} + ${operator} + ${calcNumber}`;
     outputResult(currentResult, calcDescription);
 }
 
+function add()
+{
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    crateAndWriteOutput('+', initialResult, enteredNumber)
+}
+
+
+function subtract()
+{
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    crateAndWriteOutput('-', initialResult, enteredNumber)
+}
+
+function multiply()
+{
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    crateAndWriteOutput('*', initialResult, enteredNumber)
+}
+
+function divide()
+{
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    crateAndWriteOutput('/', initialResult, enteredNumber)
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 
